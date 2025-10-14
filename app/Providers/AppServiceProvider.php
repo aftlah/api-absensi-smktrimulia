e416f6cad1d378c48bee7aac650e464117a66380
aftlah <altaf.fattah05@gmail.com>
2025-10-14 08:51:33 +0700

    fix: register GlobalActivityObserver for models in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\{Siswa, GuruPiket, Kelas, Akun, Admin, WaliKelas, JadwalPiket, Absensi, Pengaturan};
+use App\Observers\GlobalActivityObserver;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,8 +20,20 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
+
+
     public function boot(): void
     {
-        //
+        // Daftarkan observer untuk setiap model yang ingin diawasi
+        Akun::observe(GlobalActivityObserver::class);
+        Siswa::observe(GlobalActivityObserver::class);
+        GuruPiket::observe(GlobalActivityObserver::class);
+        Kelas::observe(GlobalActivityObserver::class);
+        Admin::observe(GlobalActivityObserver::class);
+        WaliKelas::observe(GlobalActivityObserver::class);
+        JadwalPiket::observe(GlobalActivityObserver::class);
+        Absensi::observe(GlobalActivityObserver::class);
+        Pengaturan::observe(GlobalActivityObserver::class);
     }
+
 }
