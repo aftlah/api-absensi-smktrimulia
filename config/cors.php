0dd3071caa4fa11b341d5919e8cfead056bc3e70
Fardias <dias.fathan2005@gmail.com>
2026-02-18 15:46:07 +0700

    feat: add new admin modules and update CORS configuration

--- config/cors.php
+++ config/cors.php
@@ -25,10 +25,9 @@ return [
         'http://localhost:5173',
         'http://localhost:5174',
         'http://127.0.0.1:3000',
+        'http://127.0.0.1:8000',
         'http://127.0.0.1:5173',
         'http://163.61.58.210'
-        // Tambahkan domain production di sini
-        // 'https://yourdomain.com',
     ],
 
     'allowed_origins_patterns' => [],
